e3e3e27fbdccee7da07120a65717946c3a1e280d
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-10 20:07:19 +0500

    Add top and side bar in the dashboard and dashboard to the app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginComponent from "./components/LoginPage";
 import RegisterComponent from "./components/RegisterPage";
+import DashboardComponent from "./components/Dashboard";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route Component={LoginComponent} path="/login" />
           <Route Component={RegisterComponent} path="/register" />
+          <Route Component={DashboardComponent} path="/dashboard" />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import TopBar from "./TopBar";
+
+const DashboardComponent = () => {
+  return (
+    <TopBar
+      username="John Doe"
+      profileImageUrl="https://source.unsplash.com/random/150x150"
+    />
+  );
+};
+
+export default DashboardComponent;
