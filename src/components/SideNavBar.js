24fd2da12d7d0dc0241917e4f0e2726e722356d0
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-11 15:46:19 +0500

    Add state to active the links when clicked.

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,49 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const SideNavbar = () => {
+  const [activeLink, setActiveLink] = useState(null);
+
   return (
     <nav className="sidebar">
       <div className="sidebar-sticky">
         <ul className="nav flex-column">
           <li className="nav-item">
-            <NavLink to={"dashboard"} className="nav-link">
+            <NavLink
+              to={"dashboard"}
+              className={`nav-link ${
+                activeLink == "dashboard" ? "active-me" : ""
+              }`}
+              onClick={() => {
+                setActiveLink("dashboard");
+              }}
+            >
               <i className="fas fa-tachometer-alt me-2"></i>
               Dashboard
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={"read4u"} className="nav-link">
+            <NavLink
+              to={"read4u"}
+              className={`nav-link ${
+                activeLink == "read4u" ? "active-me" : ""
+              }`}
+              onClick={() => {
+                setActiveLink("read4u");
+              }}
+            >
               <i className="fas fa-book-open me-2"></i>
               Read
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={"voice-artists"} className="nav-link">
+            <NavLink
+              to={"voice-artists"}
+              className={`nav-link ${activeLink == "voice" ? "active-me" : ""}`}
+              onClick={() => {
+                setActiveLink("voice");
+              }}
+            >
               <i className="fas fa-microphone me-2"></i>
               Voice Artists
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={"read-by-u"} className="nav-link">
+            <NavLink
+              to={"read-by-u"}
+              className={`nav-link ${activeLink == "pdfs" ? "active-me" : ""}`}
+              onClick={() => {
+                setActiveLink("pdfs");
+              }}
+            >
               <i className="fas fa-file-pdf me-2"></i>
               PDFs
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={"word-list"} className="nav-link">
+            <NavLink
+              to={"word-list"}
+              className={`nav-link ${activeLink == "words" ? "active-me" : ""}`}
+              onClick={() => {
+                setActiveLink("words");
+              }}
+            >
               <i className="fas fa-highlighter me-2"></i>
               Marked Words
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={"profile-setting"} className="nav-link">
+            <NavLink
+              to={"profile-setting"}
+              className={`nav-link ${
+                activeLink == "setting" ? "active-me" : ""
+              }`}
+              onClick={() => {
+                setActiveLink("setting");
+              }}
+            >
               <i className="fas fa-cog me-2"></i>
               Setting
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={"logout"} className="nav-link">
+            <NavLink
+              to={"logout"}
+              className={`nav-link ${
+                activeLink == "logout" ? "active-me" : ""
+              }`}
+              onClick={() => {
+                setActiveLink("logout");
+              }}
+            >
               <i className="fas fa-sign-out-alt me-2"></i>
               Logout
             </NavLink>
