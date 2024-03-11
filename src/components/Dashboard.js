85b94c685c0b0a4b928741db884810e9a0ed96db
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-11 10:12:01 +0500

    Add Top bar and side bar to the dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,30 @@
+import { Outlet } from "react-router";
 import TopBar from "./TopBar";
+import SideNavbar from "./SideNavBar";
+import { useState } from "react";
 
 const DashboardComponent = () => {
+  const [isTrue, setIsTrue] = useState(false);
   return (
-    <TopBar
-      username="John Doe"
-      profileImageUrl="https://source.unsplash.com/random/150x150"
-    />
+    <>
+      <TopBar
+        username="John Doe"
+        profileImageUrl="https://source.unsplash.com/random/150x150"
+        setHamStatus={setIsTrue}
+      />
+      <div className="container-fluid h-100">
+        <div className="row">
+          <div className={`${isTrue ? "col-md-3 col-lg-2" : ""}`}>
+            {isTrue && <SideNavbar />}
+          </div>
+          <div
+            className={`${isTrue ? "col-md-9 col-lg-10 dash-con" : "dash-con"}`}
+          >
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
