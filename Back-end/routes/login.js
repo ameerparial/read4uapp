84645cd5915849e8a83a175295e90ab3b946f9ef
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-13 13:11:51 +0500

    Display the data to the client side.

--- Back-end/routes/login.js
+++ Back-end/routes/login.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const Router = express.Router();
 
-const LoginRouter = () => {
-  Router.get("/", (req, res) => {
-    res.send("Ameer Hamza is here");
+const LoginRouter = (UserServices) => {
+  Router.get("/", async (req, res) => {
+    const data = await UserServices.getData();
+    console.log(data);
+    res.send(data);
   });
   return Router;
 };
