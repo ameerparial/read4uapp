b9ae31270a8d5e2550e1de768397411a4757881f
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-13 13:11:23 +0500

    Import service class in server.

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import LoginRouter from "./routes/login";
+import UserDataService from "./services/UserdataService";
+const UserServices = new UserDataService("./data/RegisterAccounts.json");
 const app = express();
 
-app.use("/login-me", LoginRouter());
+app.use("/login-me", LoginRouter(UserServices));
 
 app.listen(5500, () => {
   console.log("Listening at the port 5000...");
