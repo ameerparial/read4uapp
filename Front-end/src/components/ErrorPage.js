5c6fa4c546ab6c61a66f4b7919688a8507284532
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-15 10:53:35 +0500

    Add class flex and add the key inside map filter.

--- Front-end/src/components/ErrorPage.js
+++ Front-end/src/components/ErrorPage.js
@@ -2,10 +2,10 @@ const ErrorComponent = ({ errors }) => {
   if (errors && errors.length > 0)
     return (
       <>
-        <div class="alert alert-danger">
+        <div className="alert alert-danger d-flex p-0 m-0">
           <ul>
-            {errors.map((err) => (
-              <li>{err}</li>
+            {errors.map((err, index) => (
+              <li key={index}>{err}</li>
             ))}
           </ul>
         </div>
