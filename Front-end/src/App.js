4da0d18f0005baeb960c73cfa05326b3e474b37e
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-17 19:33:41 +0500

    Add LoginComponent on home endpoint.

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -3,14 +3,13 @@ import LoginComponent from "./components/LoginPage";
 import RegisterComponent from "./components/RegisterPage";
 import DashboardComponent from "./components/Dashboard";
 import Container from "./components/Data-Container";
-import HomePage from "./components/Home/HomePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route element={<HomePage />} path="/" />
+          <Route element={<LoginComponent />} path="/" />
           <Route Component={LoginComponent} path="/login" />
           <Route Component={RegisterComponent} path="/register" />
           <Route path="dashboard" element={<DashboardComponent />}>
