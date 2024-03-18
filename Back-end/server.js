29e3db81d418ffbb6dbb3d54727fba2f9c0cc493
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-18 12:26:58 +0500

    Add middleware cors and set the port number

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import LoginRouter from "./routes/login";
 import UserDataService from "./services/UserdataService";
+import cors from "cors";
 import bodyParser from "body-parser";
 const UserServices = new UserDataService("./data/RegisterAccounts.json");
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/login-me", LoginRouter(UserServices));
 
 app.listen(5500, () => {
-  console.log("Listening at the port 5000...");
+  console.log("Listening at the port 5500...");
 });
