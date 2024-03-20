c89ae96a891ce10afd6007bef2f7c36fdaea1b1e
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-20 11:23:37 +0500

    Add functionality to check whether account already exist or not.

--- Back-end/services/UserdataService.js
+++ Back-end/services/UserdataService.js
@@ -24,14 +24,27 @@ class UserDataService {
     return false;
   }
 
-  async addData(newUser) {
-    const { users } = await this.getData();
-    users.push(newUser);
-    console.log(users);
-    if (fs.writeFileSync(this.path, JSON.stringify({ users }))) {
+  isAlreadyExist(users, newUser) {
+    const result = users.find((user) => {
+      if (user.email === newUser.email || user.username === newUser.username) {
+        return true;
+      }
+    });
+
+    if (result) {
       return true;
     }
     return false;
+  }
+
+  async addData(newUser) {
+    const { users } = await this.getData();
+    if (this.isAlreadyExist(users, newUser)) {
+      return { status: false, msg: "Account Already exists!" };
+    }
+    users.push(newUser);
+    fs.writeFileSync(this.path, JSON.stringify({ users }));
+    return { status: true, msg: "Account Created Successfully." };
   }
 
   async getData() {
