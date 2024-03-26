ed97b2a5dcdc70ef612e84ddb1a825d82da73bad
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-26 14:41:24 +0500

    Set session and cookie on server.

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,9 +3,19 @@ import LoginRouter from "./routes/login";
 import UserDataService from "./services/UserdataService";
 import cors from "cors";
 import bodyParser from "body-parser";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 const UserServices = new UserDataService("./data/RegisterAccounts.json");
 const app = express();
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "ameerhamza",
+    cookie: { maxAge: 3000 },
+    saveUninitialized: false,
+  })
+);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
