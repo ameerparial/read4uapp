ccbfe0b6981a8fec6b5e1b2a0a9b8eea3eaeecfa
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-27 12:08:39 +0500

    Set the Cors and session properly.

--- Back-end/server.js
+++ Back-end/server.js
@@ -4,10 +4,20 @@ import UserDataService from "./services/UserdataService";
 import cors from "cors";
 import bodyParser from "body-parser";
 import session from "express-session";
+import DashboardRouter from "./routes/DashboardRoute";
 
 const UserServices = new UserDataService("./data/RegisterAccounts.json");
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   session({
     secret: "ameerhamzakhuhro",
@@ -15,10 +25,8 @@ app.use(
     resave: false,
   })
 );
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/login-me", LoginRouter(UserServices));
+app.use("/dashboard", DashboardRouter());
 
 app.listen(5500, () => {
   console.log("Listening at the port 5500...");
