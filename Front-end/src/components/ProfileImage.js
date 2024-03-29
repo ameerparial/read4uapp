2790eb1f63c821cba01169be6ec0722e0d1e1869
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-29 16:26:16 +0500

    Create the Circular Image Component to get the image.

---
+++ Front-end/src/components/ProfileImage.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import noProfileImage from "../images/profile-image.jpeg";
+
+const ProfileImage = () => {
+  const [profileImage, setProfileImage] = useState(null);
+  const onChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setProfileImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  return (
+    <div className="circular-image">
+      <div className="image-display">
+        <a>
+          <img
+            src={profileImage != null ? profileImage : noProfileImage}
+            alt="no image found!"
+          />
+        </a>
+      </div>
+      <div className="image-input">
+        <input
+          type="file"
+          id="profile-image"
+          accept="image/*"
+          style={{ display: "none" }}
+          onChange={onChange}
+        />
+        <label htmlFor="profile-image" className="btn" id="geLabel">
+          Set Profile Image
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileImage;
