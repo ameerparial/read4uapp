25303962dbd707252762f6cebeafd8410db93ed5
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-31 13:14:28 +0500

    Develop the home page.

--- Front-end/src/components/Home/HomePage.js
+++ Front-end/src/components/Home/HomePage.js
@@ -1,9 +1,24 @@
-const HomeComponent = () => {
+const HomePage = () => {
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <div id="banner" className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div id="banner-title">
+              <h1 className="title" id="heading">
+                Welcome to Read4U APP
+              </h1>
+              <p className={`lead`} id="ban-para">
+                Immerse yourself in the world of literature with our innovative
+                book and text reader application. Whether it's PDFs or plain
+                text, experience the joy of reading at your fingertips.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default HomeComponent;
+export default HomePage;
