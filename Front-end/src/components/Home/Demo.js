12247a8822040a56e4ac55a88595b29916891777
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-03-31 13:16:43 +0500

    Create the components of Home Page.

---
+++ Front-end/src/components/Home/Demo.js
@@ -0,0 +1,13 @@
+const Demo = () => {
+  return (
+    <div className="container section" id="demo">
+      <div className="row">
+        <div className="col-sm-12">
+          <h2 className="section-head">Demo</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Demo;
---
+++ Front-end/src/components/Home/Service.js
@@ -0,0 +1,46 @@
+const Service = () => {
+  return (
+    <div className="container section" id="services">
+      <div className="row">
+        <div className="col-sm-12">
+          <h2 className="section-head">Service</h2>
+        </div>
+
+        <div className="col-md-4">
+          <div className="service-box">
+            <i className="fas fa-cog"></i>
+            <h2 className="lead">Some heading</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+              voluptatibus!
+            </p>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="service-box">
+            <i className="fas fa-cog"></i>
+            <h2 className="lead">Some heading</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+              voluptatibus!
+            </p>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="service-box">
+            <i className="fas fa-cog"></i>
+            <h2 className="lead">Some heading</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+              voluptatibus!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
