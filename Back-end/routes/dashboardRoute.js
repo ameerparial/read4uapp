679926233edbe695002964fc5d77a06cbf64be1e
Ameer Hamza Khuhro <khuhroameerhamza4@gmail.com>
2024-04-06 12:09:25 +0500

    Create a route to get the data and update it.

--- Back-end/routes/dashboardRoute.js
+++ Back-end/routes/dashboardRoute.js
@@ -1,13 +1,29 @@
 import express from "express";
 const Router = express.Router();
 
-const DashboardRouter = () => {
+const DashboardRouter = (profileUploader, userServices) => {
   Router.get("/", (req, res) => {
     const data = req.session.user;
     console.log("Session Data has: ..");
     console.log(data);
     return res.json(data);
   });
+
+  Router.post(
+    "/profileUpdate",
+    profileUploader.single("file"),
+    async (req, res) => {
+      const filename = req?.file?.filename;
+      var updateUser = req.body;
+      if (req?.file) updateUser = { ...updateUser, filename };
+      // console.log("update object: ");
+      // console.log(updateUser);
+      // console.log("session");
+      req.session.user = updateUser;
+      const status = await userServices.updateUser(updateUser);
+      res.json(status);
+    }
+  );
 
   return Router;
 };
